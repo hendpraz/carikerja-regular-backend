64a022728a3c4dbbc66bcb66fff6055f4a1e85fe
hendpraz <mhendryp99@gmail.com>
2020-06-07 20:16:45 +0700

    Add response for welcome intent

--- handler.js
+++ handler.js
@@ -1,10 +1,51 @@
 const line = require('@line/bot-sdk');
+const https = require('https');
+
+const executeWitAI = async (incomingMessage) => {
+  // Process to Wit.ai
+  let url = process.env.WIT_AI_URL;
+  url += `q=${incomingMessage}`;
+
+  const options = {
+    headers: {
+      "Authorization" : `Bearer ${process.env.WIT_AI_AUTHTOKEN}`
+    }
+  };
+  return await new Promise(async (resolve, reject) => {
+      https.get(url, options, async function(res) {
+        console.log("Got response: " + res.statusCode);
+        const replyMessage = {
+          type: 'text',
+          text: `Pesan Anda: ${incomingMessage}`
+        };
+        res.on('data', function (chunk) {
+          const data = JSON.parse(chunk);
+          const intentName = data.intents[0].name;
+
+          switch(intentName) {
+            case 'Default_Welcome_Intent':
+              replyMessage.text = 'Halo juga';
+              break;
+            default:
+              console.log(`Invalid intent name: ${intentName}`);
+          }
+
+          console.log('Done!');
+        });
+        resolve(replyMessage);
+      }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+        reject(e);
+      });
+  });
+};
 
 export const handle = async (event, context) => {
   // Validate that the request is from your LINE Channel first
 
+
   // Get channelAccessToken from env variables
-  const channelAccessToken = process.env.ACCESS_TOKEN;
+  const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
 
   console.log(event);
   const data = JSON.parse(event.body);
@@ -12,22 +53,21 @@ export const handle = async (event, context) => {
   console.log(lineEvent);
 
   const replyToken = lineEvent.replyToken;
-  const incomeMessage = lineEvent.message.text;
+  const incomingMessage = lineEvent.message.text;
 
+  const replyMessage = await executeWitAI(incomingMessage);
+
+  // Reply to client
   const client = new line.Client({
     channelAccessToken: channelAccessToken
   });
 
-  const replyMessage = {
-    type: 'text',
-    text: `Pesan Anda: ${incomeMessage}`
-  };
+  console.log(replyToken);
+  console.log(replyMessage);
 
   await client.replyMessage(replyToken, replyMessage)
     .then(() => {
-      console.log('OK');
-      console.log(replyToken);
-      console.log(replyMessage);
+      console.log('OK, reply sent');
     })
     .catch((err) => {
       console.error(err);
