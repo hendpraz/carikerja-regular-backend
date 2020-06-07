eceac834ca26eb0a0c82652d9257035876147c6f
hendpraz <mhendryp99@gmail.com>
2020-06-07 12:41:16 +0700

    Get parameters from dialogflow query: jobfinding

--- jobfinding.js
+++ jobfinding.js
@@ -1,4 +1,11 @@
 export const findjobs = async (event, context) => {
+  console.log(event.body);
+  const data = JSON.parse(event.body);
+  const location = data.queryResult.parameters['geo-city'] || 'None';
+  const profession = data.queryResult.parameters.Profession || 'None';
+
+  console.log(location);
+  console.log(profession);
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -6,7 +13,7 @@ export const findjobs = async (event, context) => {
         {
           "text": {
             "text": [
-              "Example jobs from serverless API: 1 2 3"
+              `Contoh pekerjaan dari serverless API: \n1. Kerja A - CP 08xx \n2. Kerja B - CP 08xx \n3. Kerja B - CP 08xx \n\nFilter diterima: Lokasi-${location}; Profesi-${profession};`
             ]
           }
         }
