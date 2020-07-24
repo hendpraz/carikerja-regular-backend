afca496a6ef1d98fe8bad9fea4da02755b1d9361
hendpraz <mhendryp99@gmail.com>
2020-07-25 01:55:09 +0700

    Add 'update village job status/detail', 'complete village job', fix routes

--- handlers/villagejob.js
+++ handlers/villagejob.js
@@ -1,5 +1,6 @@
 import handler from "../libs/handler-lib";
-import VillageJob from '../models/VillageJob';
+import VillageJob from "../models/VillageJob";
+import VillageAcceptance from "../models/VillageAcceptance";
 import { validateAdmin } from "../libs/villagevalidator";
 
 export const createVillageJob = handler(async (event, context) => {
@@ -54,4 +55,89 @@ export const getVillageJob = handler(async (event, context) => {
   }
 
   return { message: "OK", job: foundJob };
+});
+
+export const updateVillageJobDetail = handler(async (event, context) => {
+  console.log(event.body);
+  const data = JSON.parse(event.body);
+
+  const jobId = event.pathParameters.idj;
+  const villageId = event.pathParameters.idv;
+
+  // Validate User First
+  const identityId = event.requestContext.identity.cognitoIdentityId;
+  await validateAdmin(identityId, villageId);
+
+  const foundJob = await VillageJob.findById(jobId);
+
+  if (!foundJob) {
+    throw new Error("Job not found");
+  }
+
+  foundJob.title = data.title;
+  foundJob.owner = data.owner;
+  foundJob.description = data.description;
+  foundJob.num_of_openings = data.num_of_openings;
+  foundJob.status = 'active';
+  foundJob.location = data.location;
+  foundJob.profession = data.profession;
+
+  await foundJob.save();
+
+  return { message: "OK" };
+});
+
+export const updateVillageJobStatus = handler(async (event, context) => {
+  console.log(event.body);
+  const data = JSON.parse(event.body);
+
+  const jobId = event.pathParameters.idj;
+  const villageId = event.pathParameters.idv;
+
+  // Validate User First
+  const identityId = event.requestContext.identity.cognitoIdentityId;
+  await validateAdmin(identityId, villageId);
+
+  const foundJob = await VillageJob.findById(jobId);
+
+  if (!foundJob) {
+    throw new Error("Job not found");
+  }
+
+  if ((data.status === "active") || (data.status === "inactive")) {
+    foundJob.status = data.status;
+  } else {
+    throw new Error("Invalid job status");
+  }
+
+  await foundJob.save();
+
+  return { message: "OK" };
+});
+
+export const completeVillageJob = handler(async (event, context) => {
+  const jobId = event.pathParameters.idj;
+  const villageId = event.pathParameters.idv;
+
+  // Validate User First
+  const identityId = event.requestContext.identity.cognitoIdentityId;
+  await validateAdmin(identityId, villageId);
+
+  const foundJob = await VillageJob.findById(jobId);
+
+  if (!foundJob) {
+    throw new Error("Job not found");
+  }
+
+  const newVillageAcceptance = {};
+
+  foundJob.status = "inactive";
+  newVillageAcceptance.village_user = data.village_user;
+  newVillageAcceptance.village_job = jobId;
+  newVillageAcceptance.date = Date.now();
+
+  await foundJob.save();
+  await VillageAcceptance.create(newVillageAcceptance);
+
+  return { message: "OK" };
 });
