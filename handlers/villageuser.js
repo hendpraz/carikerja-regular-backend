29fd6dd14d9ff47f5099328a95be26f15a9f1f69
hendpraz <mhendryp99@gmail.com>
2020-07-24 19:53:31 +0700

    Update 'create' and 'update' village user handler

--- handlers/villageuser.js
+++ handlers/villageuser.js
@@ -131,17 +131,35 @@ export const revokeVillageAdmin = handler(async (event, context) => {
   return { message: "OK" };
 });
 
-export const createVillageUser  = handler(async (event, context) => {  
+export const createVillageUser = handler(async (event, context) => {  
   console.log(event.body);
   const data = JSON.parse(event.body);
   const identityId = event.requestContext.identity.cognitoIdentityId;
   // Validate User First
   validateAdmin(identityId, data.village);
 
+  const newUser = {};
+
+  newUser.name = data.name;
+  newUser.phone_number = data.phone_number;
+  newUser.address = data.address;
+  newUser.status = 'active';
+  newUser.subscription_plan = VILLAGE_USER;
+  newUser.village = data.village;
+
+  // TODO: Register Cognito User
+  // username = phone_number
+  // password = password
+
+  const userId;
+  newUser.user_id = userId;
+
+  await VillageUser.create(newUser);
+
   return { message: "OK" };
 });
 
-export const getVillageUser  = handler(async (event, context) => {  
+export const getVillageUser = handler(async (event, context) => {  
   console.log(event.body);
   const data = JSON.parse(event.body);
   const identityId = event.requestContext.identity.cognitoIdentityId;
@@ -175,6 +193,26 @@ export const listVillageUser  = handler(async (event, context) => {
   return { message: "OK", user_list: foundUser };
 });
 
-export const updateVillageUser  = handler(async (event, context) => {  
+export const updateVillageUser = handler(async (event, context) => {  
+  console.log(event.body);
+  const data = JSON.parse(event.body);
+  const identityId = event.requestContext.identity.cognitoIdentityId;
+  // Validate User First
+  validateAdmin(identityId, data.village);
+
+  // await connectToDatabase();
+  const foundUser = await VillageUser.findOne(
+    { user_id: data.user_id, subscription_plan: VILLAGE_USER, village: data.village }
+  );
+
+  if (!foundUser) {
+    throw new Error("User not found");
+  }
+
+  foundUser.name = data.name;
+  foundUser.phone_number = data.phone_number;
+  foundUser.address = data.address;
+  await foundUser.save();
+
   return { message: "OK" };
 });
